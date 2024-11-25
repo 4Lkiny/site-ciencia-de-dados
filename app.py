b11fdc310d0e33be49a757fda75e6275132a11f5
4Lkiny <wendyescarza2007@gmail.com>
2024-11-25 08:03:25 -0300

    .

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(_name_,
-            template_folder= "siteciencia/templates", 
+app = Flask(__name__, template_folder= "siteciencia/templates", 
             static_folder = "siteciencia/static")
 @app.route("/")
 def index():
     #return "<h1> Bom dia, professor.<h1/>"
-    return render_template("Index.html")
+    return render_template("index.html")
+
+@app.route ("analise", dafaults= {'usuario':"Visitante"})
+@app.route("/analise/<usuario>")
+def analise(usuario):
+    return render_template("analise.html", usuario=usuario)
+if __name__=="__main__":
+    app.run (debug=True)
